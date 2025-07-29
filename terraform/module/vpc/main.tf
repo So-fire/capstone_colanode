095ad39e53daa617d2a31a59228a5ae9eb4c69d8
sophia-riv <sophia.udofiah@rivemation.com>
2025-07-29 10:01:42 +0100

    testing for vpc

--- terraform/module/vpc/main.tf
+++ terraform/module/vpc/main.tf
@@ -170,7 +170,7 @@ resource "aws_route_table" "database" {
 # Public Route Table Associations
 ##=====================================================
 resource "aws_route_table_association" "public" {
-  count          = 2
+  count          = 1
   subnet_id      = aws_subnet.public[count.index].id
   route_table_id = aws_route_table.public.id
 }
@@ -179,7 +179,7 @@ resource "aws_route_table_association" "public" {
 # Private Route Table Associations
 ##============================================================
 resource "aws_route_table_association" "private" {
-  count          = 2
+  count          = 1
   subnet_id      = aws_subnet.private[count.index].id
   route_table_id = aws_route_table.private.id
 }
@@ -188,7 +188,7 @@ resource "aws_route_table_association" "private" {
 # Database Route Table Associations
 ##============================================================
 resource "aws_route_table_association" "database" {
-  count          = 2
+  count          = 1
   subnet_id      = aws_subnet.database[count.index].id
   route_table_id = aws_route_table.database.id
 }
