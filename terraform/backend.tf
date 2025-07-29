9701b7c474bc4d04fabcc079e4cb96181ae6f7cd
sophia-riv <sophia.udofiah@rivemation.com>
2025-07-29 09:58:15 +0100

    testing for vpc

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     encrypt = true
-    bucket  = "colanode-tfstate "
+    bucket  = "colanode-tfstate"
     key     = "infra.tfstate"
     region  = "us-east-1"
     # profile        = "default"
