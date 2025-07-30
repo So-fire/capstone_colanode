90f6be725abf36c59b1333118d6d58fc08ebb13b
sophia-riv <sophia.udofiah@rivemation.com>
2025-07-30 20:43:49 +0100

    testing for RDS

--- terraform/module/rds_postgresql/main.tf
+++ terraform/module/rds_postgresql/main.tf
@@ -13,7 +13,7 @@ resource "aws_db_instance" "this" {
   allocated_storage     = var.allocated_storage
   max_allocated_storage = var.max_allocated_storage
   engine                = "postgres"
-  engine_version        = "15.3"
+  engine_version        = "15.5"
   instance_class        = var.instance_class
   db_name               = var.db_name
   username              = var.db_username
