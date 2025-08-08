a20c07a69e148b23fc93e384547d42d22203ae04
sophia-riv <sophia.udofiah@rivemation.com>
2025-08-08 21:57:18 +0100

    testing script tofetc db credential to ec2 fixing deployment waala

--- terraform/module/policy/main.tf
+++ terraform/module/policy/main.tf
@@ -26,7 +26,8 @@ resource "aws_iam_policy" "private_ec2_policy" {
         Effect = "Allow"
         Action = [
           "secretsmanager:GetSecretValue",
-          "secretsmanager:DescribeSecret"
+          "secretsmanager:DescribeSecret",
+          "secretsmanager:ListSecrets"
         ]
         Resource = "*"
       },
